56c21d4234f15d5ba634205eb143493c8a3e2b29
pantelri <pantelri@hs-albsig.de>
2024-10-22 14:24:49 +0200

    minor bugfixinf

--- excel_operations/planning_writer.py
+++ excel_operations/planning_writer.py
@@ -25,12 +25,11 @@ class Planning():
 
         # Startpunkt für die Tabelle (3 Zeilen unter dem Startzellenpunkt)
         current_row = int(start_cell[1:]) + 3
-        current_col = ord(start_cell[0]) - ord('A')
 
         # Schreibe die Überschriften
-        self.planning_sheet[get_cell_name(start_cell, 0, 3)] = "Fiscal Year"
-        self.planning_sheet[get_cell_name(start_cell, 1, 3)] = "Sum"
-        self.planning_sheet[get_cell_name(start_cell, 2, 3)] = "Change vs PY"
+        self.planning_sheet[get_cell_name(start_cell, 0, 2)] = "Fiscal Year"
+        self.planning_sheet[get_cell_name(start_cell, 1, 2)] = "Annual total"
+        self.planning_sheet[get_cell_name(start_cell, 2, 2)] = "Change compared to PY"
 
         prev_value = None
         for fiscal_year, value in grouped_data.items():
@@ -43,6 +42,7 @@ class Planning():
             # Berechne und schreibe Veränderung zum Vorjahr
             if prev_value is not None:
                 change = value - prev_value
+                change = change / prev_value
                 self.planning_sheet[get_cell_name(start_cell, 2, current_row - int(start_cell[1:]))] = change
             
             prev_value = value
